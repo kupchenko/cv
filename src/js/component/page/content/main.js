65e5ec1979e33545122f1c3a11f7e2ce4a50e81e
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2020-03-16 14:39:04 +0200

    Add download cv

--- src/js/component/page/content/main.js
+++ src/js/component/page/content/main.js
@@ -3,14 +3,24 @@ import {Divider} from 'antd';
 import Experience from '@/js/component/page/content/experience';
 import Summary from '@/js/component/page/content/summary';
 import {connect} from 'react-redux';
+import CloudDownloadOutlined from "@ant-design/icons/lib/icons/CloudDownloadOutlined";
 
 class MainContent extends PureComponent {
+
+    cvFileName = 'CV_Dmitrii_Kupchenko_Senior_Java_Software_Engineer.pdf';
 
     render() {
         const {name, title} = this.props;
         return (
             <>
-                <div className='employee-name'>{name}</div>
+                <div className='cv-name-header-row'>
+                    <div className='employee-name'>{name}</div>
+                    <a className='cv-download-link'
+                       href='../../../../files/cv.pdf'
+                       download={this.cvFileName}><CloudDownloadOutlined/> Download
+                        CV</a>
+
+                </div>
                 <div className='employee-title'>{title}</div>
 
                 <Divider orientation='left'>Summary</Divider>
